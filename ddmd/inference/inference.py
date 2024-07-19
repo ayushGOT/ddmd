44a418047f18778cd94e197736d001cf06ab44bb
Ayush Gupta <agupta55@carya.rcdc.uh.edu>
2024-07-18 21:36:41 -0500

    added 'nudge' variable,changed logic to select latest CVAE,modified LOFs < -100

--- ddmd/inference/inference.py
+++ ddmd/inference/inference.py
@@ -4,10 +4,12 @@ import json
 import time
 import pandas as pd
 import MDAnalysis as mda
+import numpy as np
 
 from typing import List
 from MDAnalysis.analysis import rms
 from MDAnalysis.analysis import distances
+from MDAnalysis.analysis import contacts
 from sklearn.neighbors import LocalOutlierFactor
 from tqdm import tqdm
 
@@ -15,6 +17,39 @@ from ddmd.ml import ml_base
 from ddmd.utils import build_logger, dict_from_yaml, get_numoflines, get_dir_base
 
 logger = build_logger()
+
+## for Q calculation ###
+'''
+q1= [15.,16.,16.,16.,16.,17.,17.,17.,17.,17.,17.,18.,18.,18.,18.,18.,20.,20.,
+ 20.,20.,21.,21.,21.,21.,49.,49.,49.,50.,50.,50.,50.,50.,50.,50.,50.,50.,
+ 51.,51.,51.,51.,51.,51.,51.,51.,51.,51.,51.,51.,52.,52.,52.,52.,52.,52.,
+ 52.,52.,52.,52.,52.,52.,52.,52.,54.,54.,54.,54.,54.,54.,54.,54.,54.,55.,
+ 55.,55.,55.,55.,55.,55.,55.,55.,55.,58.,58.,82.,84.,84.,84.,84.,84.,84.,
+ 84.,85.,85.,85.,85.,85.,85.,85.,88.,88.,88.,88.,88.,88.,88.,88.,89.,89.,
+ 89.,89.,89.,90.,90.,90.]    # to store atom no. 1 from the pair of atoms
+
+q2= [315.,314.,315.,318.,319.,284.,312.,314.,315.,318.,319.,314.,315.,318.,
+ 319.,320.,314.,318.,319.,320.,314.,318.,319.,320.,284.,315.,318.,250.,
+ 283.,284.,287.,288.,314.,315.,318.,319.,249.,250.,251.,281.,283.,284.,
+ 287.,288.,312.,314.,315.,318.,250.,251.,283.,284.,287.,288.,289.,309.,
+ 312.,314.,315.,318.,319.,320.,283.,284.,287.,288.,289.,314.,318.,319.,
+ 320.,254.,283.,287.,288.,289.,309.,314.,318.,319.,320.,318.,320.,250.,
+ 249.,250.,251.,253.,254.,284.,287.,248.,249.,250.,251.,283.,284.,287.,
+ 249.,250.,251.,253.,254.,283.,284.,287.,249.,250.,251.,253.,254.,251.,
+ 253.,254.]      # to store atom no. 2 from the pair of atoms
+
+ref= [0.744,0.621,0.539,0.553,0.721,0.733,0.743,0.536,0.408,0.528,0.722,0.564,
+ 0.551,0.436,0.565,0.565,0.736,0.577,0.65 ,0.589,0.747,0.56 ,0.569,0.462,
+ 0.745,0.737,0.703,0.713,0.618,0.538,0.553,0.72 ,0.622,0.579,0.602,0.73 ,
+ 0.713,0.628,0.736,0.742,0.534,0.408,0.529,0.723,0.742,0.592,0.495,0.623,
+ 0.737,0.689,0.563,0.552,0.437,0.567,0.566,0.738,0.695,0.589,0.606,0.564,
+ 0.645,0.709,0.734,0.749,0.575,0.649,0.589,0.71 ,0.639,0.662,0.68 ,0.729,
+ 0.746,0.56 ,0.569,0.462,0.704,0.749,0.689,0.664,0.686,0.739,0.73 ,0.741,
+ 0.619,0.534,0.564,0.736,0.747,0.717,0.732,0.746,0.538,0.408,0.551,0.717,
+ 0.61 ,0.716,0.551,0.527,0.435,0.575,0.56 ,0.731,0.715,0.664,0.72 ,0.722,
+ 0.566,0.65 ,0.57 ,0.564,0.587,0.461]  # to store the native distances (already mutiplied by 1.5) b/w the pair of atoms
+'''
+#####
 
 class inference_run(ml_base): 
     """
@@ -62,11 +97,11 @@ class inference_run(ml_base):
         df_entry = []
         if ref_pdb: 
             ref_u = mda.Universe(ref_pdb)
-            sel_ref = ref_u.select_atoms(atom_sel)
+            sel_ref = ref_u.select_atoms(atom_sel)    # atom selection to calculate RMSD
         vae_config = self.get_cvae(dry_run=True)
-        cm_sel = vae_config['atom_sel'] if 'atom_sel' in vae_config else 'name CA'
+        cm_sel = vae_config['atom_sel'] if 'atom_sel' in vae_config else 'name CA'   # atom selection for contact maps
         cm_cutoff = vae_config['cutoff'] if 'cutoff' in vae_config else 8
-        logger.info("Processing MD trajectories.")
+        logger.info("Processing MD trajectories")
         cm_list = []
         for dcd in tqdm(dcd_files): 
             setup_yml = f"{os.path.dirname(dcd)}/setting.yml"
@@ -77,7 +112,7 @@ class inference_run(ml_base):
                 logger.info(f"Skipping {dcd}...")
                 continue
 
-            sel_atoms = mda_u.select_atoms(atom_sel)
+            sel_atoms = mda_u.select_atoms(atom_sel)  # atom selection to calculate RMSD
             sel_cm = mda_u.select_atoms(cm_sel)
             for ts in mda_u.trajectory: 
                 cm = (distances.self_distance_array(sel_cm.positions) < cm_cutoff) * 1.0
@@ -92,6 +127,15 @@ class inference_run(ml_base):
                     local_entry['rmsd'] = rmsd
                 # possible new analysis
                 df_entry.append(local_entry)
+                
+                ## Calculating and storing Q
+#                r= np.array([])
+ #               for i in range(114):
+  #                  atom1, atom2 =mda_u.select_atoms("bynum %i" %q1[i]), mda_u.select_atoms("bynum %i" %q2[i])
+   #                 dist= distances.distance_array(atom1.positions,atom2.positions)*0.1
+    #                r=np.append(r,dist)
+     #           Q= contacts.soft_cut_q(r, ref, beta=50.0, lambda_constant=1.0)
+      #          local_entry['Q'] = Q
         
         df = pd.DataFrame(df_entry)
         if 'strides' in vae_config: 
@@ -100,6 +144,8 @@ class inference_run(ml_base):
         embeddings = self.vae.return_embeddings(vae_input)
         df['embeddings'] = embeddings.tolist()
         outlier_score = lof_score_from_embeddings(embeddings, **kwargs)
+        for i, _ in enumerate(outlier_score):
+            outlier_score[i]= outlier_score[i] if outlier_score[i] > -100 else 0
         df['lof_score'] = outlier_score
         return df
 
@@ -108,7 +154,7 @@ class inference_run(ml_base):
         getting the last model so far
         """
         # get weight 
-        vae_weight = self.get_trained_models()[-1]
+        vae_weight =  max(self.get_trained_models(), key=os.path.getctime)  # to select the latest CVAE model
         vae_setup = os.path.join(os.path.dirname(vae_weight), 'cvae.json')
         vae_label = os.path.basename(os.path.dirname(vae_weight))
         # get conf 
@@ -117,12 +163,13 @@ class inference_run(ml_base):
             self.vae, _ = self.build_vae(**vae_config, **kwargs)
             logger.info(f"vae created from {vae_setup}")
         # load weight
+        time.sleep(30)          # to allow time for writing the h5 file before it's opened
         logger.info(f" ML nn loaded weight from {vae_label}")
         self.vae.load(vae_weight)
         return vae_config
 
     def ddmd_run(self, n_outliers=50, 
-            md_threshold=0.75, screen_iter=10, **kwargs): 
+            md_threshold=0.75, screen_iter=10, nudge='no',**kwargs): 
         iteration = 0
         while True: 
             trained_models = self.get_trained_models() 
@@ -138,10 +185,19 @@ class inference_run(ml_base):
             df = self.build_md_df(**kwargs)
             logger.info(f"Built dataframe from {len(df)} frames. ")
             df_outliers = df.sort_values('lof_score').head(n_outliers)
+
             if 'ref_pdb' in kwargs: 
-                df_outliers = df_outliers.sort_values('rmsd')
+                if nudge == 'high':     # nudge towards higher RMSD
+                    df_outliers = df_outliers.sort_values(by='rmsd', ascending=False)
+                    logger.info("nudging towards higher RMSD")
+                if nudge == 'low':      # nudge towards lower RMSD
+                    df_outliers = df_outliers.sort_values(by='rmsd', ascending=True)
+                    logger.info("nudging towards lower RMSD")
                 logger.info(f"Lowest RMSD: {min(df['rmsd'])} A, "\
-                    f"lowest outlier RMSD: {min(df_outliers['rmsd'])} A. ")
+                    f"lowest outlier RMSD: {min(df_outliers['rmsd'])} A. "\
+                    f"Highest RMSD: {max(df['rmsd'])} A. " )
+       #             f"Lowest Q: {min(df['Q'])} , "\
+        #            f"Highest Q: {max(df['Q'])} ")
             # 
             if iteration % screen_iter == 0: 
                 logger.info(df_outliers.to_string())
