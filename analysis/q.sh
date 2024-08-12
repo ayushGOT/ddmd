60159a438c9db16ca45c1273396fdf1eddc2c71a
Ayush Gupta <agupta55@sabine.rcdc.uh.edu>
2024-08-12 00:20:03 -0500

    untracked files

--- analysis/q.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for md in /project/zerze/ayushg/argonne/ddmd/rna2/test/md_run/md_run*
-do
-cd "$md"
-
-plumed driver --mf_dcd ./output.dcd --plumed /project/zerze/ayushg/argonne/ddmd/analysis/plumed_driver_Q.dat --pdb /project/zerze/ayushg/argonne/ddmd/analysis/gaga_native.pdb
-done
